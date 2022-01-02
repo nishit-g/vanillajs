4d041e123ed5b720fe678ff4985c99caf21054a7
Nishit Gupta <nishit.gupta6708@gmail.com>
2022-01-02 17:00:28 +0530

    feat: persist info and repopulate UI

--- 01-fun-little-projects/02-movie-seat-booking/script.js
+++ 01-fun-little-projects/02-movie-seat-booking/script.js
@@ -4,17 +4,59 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 
 const movieSelector = document.getElementById("movie-selector");
 
+const persistSelectedSeats = (selectedSeats) => {
+  const selectedSeatsIdx = [...selectedSeats].map((selectedSeat) =>
+    [...seats].indexOf(selectedSeat)
+  );
+
+  localStorage.setItem("@selectedSeats", JSON.stringify(selectedSeatsIdx));
+};
+
+const repopulateUI = () => {
+  // Seats
+  const selectedSeatsIdx = JSON.parse(localStorage.getItem("@selectedSeats"));
+
+  if (selectedSeatsIdx !== null && selectedSeatsIdx.length > 0) {
+    seats.forEach((seat, idx) => {
+      if (selectedSeatsIdx.indexOf(idx) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  // Movie Selector
+
+  const selectedMovieIndex = localStorage.getItem("@selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    movieSelector.selectedIndex = selectedMovieIndex;
+  }
+};
+
+const persistSelecteMovie = () => {
+  localStorage.setItem("@selectedMovieIndex", movieSelector.selectedIndex);
+};
+
 const updateInfo = () => {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
   // console.log(selectedSeats);
   const selectedSeatsCount = selectedSeats.length;
+
+  persistSelectedSeats(selectedSeats);
 
   const seatsTextEle = document.getElementById("total-seats");
   seatsTextEle.innerText = selectedSeatsCount;
 
   const totalPriceEle = document.getElementById("total");
   totalPriceEle.innerText = selectedSeatsCount * movieSelector.value;
+
+  persistSelecteMovie();
 };
+
+movieSelector.addEventListener("change", () => {
+  updateInfo();
+  persistSelecteMovie();
+});
 
 seatContainer.addEventListener("click", (e) => {
   const isSelectableSeat =
@@ -26,3 +68,6 @@ seatContainer.addEventListener("click", (e) => {
     updateInfo();
   }
 });
+
+repopulateUI();
+updateInfo();
