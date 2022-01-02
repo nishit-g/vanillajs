8b7bad9bb2a78b3f98c06651edd13ae349b78ab2
Nishit Gupta <nishit.gupta6708@gmail.com>
2022-01-02 16:43:02 +0530

    feat(script): update info for user

--- 01-fun-little-projects/02-movie-seat-booking/script.js
+++ 01-fun-little-projects/02-movie-seat-booking/script.js
@@ -2,6 +2,20 @@ const seatContainer = document.querySelector(".seat-container");
 //Node list can be used as array
 const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 
+const movieSelector = document.getElementById("movie-selector");
+
+const updateInfo = () => {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  // console.log(selectedSeats);
+  const selectedSeatsCount = selectedSeats.length;
+
+  const seatsTextEle = document.getElementById("total-seats");
+  seatsTextEle.innerText = selectedSeatsCount;
+
+  const totalPriceEle = document.getElementById("total");
+  totalPriceEle.innerText = selectedSeatsCount * movieSelector.value;
+};
+
 seatContainer.addEventListener("click", (e) => {
   const isSelectableSeat =
     !e.target.classList.contains("occupied") &&
@@ -9,5 +23,6 @@ seatContainer.addEventListener("click", (e) => {
 
   if (isSelectableSeat) {
     e.target.classList.toggle("selected");
+    updateInfo();
   }
 });
